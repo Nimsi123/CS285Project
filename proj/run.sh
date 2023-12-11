850e37dd8731c7102e6ccd86fc01792428094620
Daniel Etaat <danielbetaat@berkeley.edu>
2023-12-11 12:23:52 -0800

    Set checkpoint to allow JonJon to continue run.

--- proj/run.sh
+++ proj/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # python cs285/scripts/run_sweep.py --params_file sweeps/cartpole_linear.json --num_sweeps 2
-# python cs285/scripts/run_sweep.py --params_file sweeps/cartpole_adaptive.json --num_sweeps 2
-# python cs285/scripts/run_sweep.py --params_file sweeps/cartpole_sine.json --num_sweeps 2
+python cs285/scripts/run_sweep.py --params_file sweeps/cartpole_adaptive_checkpoint.json --num_sweeps 1
+python cs285/scripts/run_sweep.py --params_file sweeps/cartpole_sine.json --num_sweeps 2
 
-python cs285/scripts/run_sweep.py --params_file sweeps/mc_linear.json --num_sweeps 2
-python cs285/scripts/run_sweep.py --params_file sweeps/mc_adaptive.json --num_sweeps 2
-python cs285/scripts/run_sweep.py --params_file sweeps/mc_sine.json --num_sweeps 2
+# python cs285/scripts/run_sweep.py --params_file sweeps/mc_linear.json --num_sweeps 2
+# python cs285/scripts/run_sweep.py --params_file sweeps/mc_adaptive.json --num_sweeps 2
+# python cs285/scripts/run_sweep.py --params_file sweeps/mc_sine.json --num_sweeps 2
+
