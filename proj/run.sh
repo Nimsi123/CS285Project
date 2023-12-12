80c79a9564d21d39bfefa70998752d6c6497a6c3
Daniel Etaat <danielbetaat@berkeley.edu>
2023-12-11 21:56:11 -0800

    Setup new batch of runs for JonJon

--- proj/run.sh
+++ proj/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+mv data data2
+mkdir data
+
 # python cs285/scripts/run_sweep.py --params_file sweeps/cartpole_linear.json --num_sweeps 2
-python cs285/scripts/run_sweep.py --params_file sweeps/cartpole_adaptive_checkpoint.json --num_sweeps 1
+# python cs285/scripts/run_sweep.py --params_file sweeps/cartpole_adaptive_checkpoint.json --num_sweeps 1
 python cs285/scripts/run_sweep.py --params_file sweeps/cartpole_sine.json --num_sweeps 2
 
 # python cs285/scripts/run_sweep.py --params_file sweeps/mc_linear.json --num_sweeps 2
-# python cs285/scripts/run_sweep.py --params_file sweeps/mc_adaptive.json --num_sweeps 2
+python cs285/scripts/run_sweep.py --params_file sweeps/mc_adaptive_checkpoint.json --num_sweeps 1
 # python cs285/scripts/run_sweep.py --params_file sweeps/mc_sine.json --num_sweeps 2
 
+python cs285/scripts/run_sweep.py --params_file sweeps/acrobot_linear.json --num_sweeps 2
+python cs285/scripts/run_sweep.py --params_file sweeps/acrobot_adaptive.json --num_sweeps 2
+python cs285/scripts/run_sweep.py --params_file sweeps/acrobot_sine.json --num_sweeps 2
